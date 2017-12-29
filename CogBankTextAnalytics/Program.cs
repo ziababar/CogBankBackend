7a30bdcd48fd5952323bd65c02714e018c0cc40c
ziababar <zbabar@gmail.com>
2017-12-28 20:55:23 -0500

    text analytics

--- CogBankTextAnalytics/Program.cs
+++ CogBankTextAnalytics/Program.cs
@@ -16,7 +16,6 @@ namespace CogBankTextAnalytics
 
         static void Main(string[] args)
         {
-            var reader = new StreamReader(@"C:\test.csv");
 
             // For this demo, the following is the Azure Text Analytics API information
             // Endpoint: https://eastus2.api.cognitive.microsoft.com/text/analytics/v2.0
@@ -24,20 +23,10 @@ namespace CogBankTextAnalytics
             // Key1: 7ab8815bff994de0a37aad20d4704b3e
             // Key2: 95a18817fe9343729fb10c9b3f3f418a
 
-
-            using ()
-            {
-                List<string> listA = new List<string>();
-                List<string> listB = new List<string>();
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    var values = line.Split(';');
-
-                    listA.Add(values[0]);
-                    listB.Add(values[1]);
-                }
-            }
+            var reader = new StreamReader(@"\data\tweets-2017-12-28-004007.csv");
+            string strLanguage;
+            string strId;
+            string strTweet;
 
             // Create a client.
             ITextAnalyticsAPI client = new TextAnalyticsAPI();
@@ -49,11 +38,35 @@ namespace CogBankTextAnalytics
             // Extracting sentiment
             Console.WriteLine("\n\n===== SENTIMENT ANALYSIS ======");
 
+            List<MultiLanguageInput> tweetList;
+            int i = 0;
+            for (reader.ReadLine(i); )
+            while (!reader.EndOfStream)
+            {
+                strId = "";
+                strLanguage = "";
+                strTweet = "";
+                
+                 
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                strLanguage = values[3];
+                strId = values[0];
+                strTweet = values[1];
+
+                tweetList[i] = new MultiLanguageInput(strLanguage, strId, strTweet);
+                tweetList.Add(new MultiLanguageInput(strLanguage, strId, strTweet));
+                i = i + 1;
+
+            }
+
             SentimentBatchResult result3 = client.Sentiment(
                     new MultiLanguageBatchInput(
 
                         new List<MultiLanguageInput>()
                         {
+
                           new MultiLanguageInput("en", "0", "I had the best day of my life."),
                           new MultiLanguageInput("en", "1", "This was a waste of my time. The speaker put me to sleep."),
                         }));
